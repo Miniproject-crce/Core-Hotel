45391901291b04538a11812a2209908df20da452
Miniproject-crce <147860515+Miniproject-crce@users.noreply.github.com>
2023-10-15 21:05:06 +0530

    Add files via upload

---
+++ suite-script.js
@@ -0,0 +1,6 @@
+/ JavaScript for Deluxe Suite Page
+
+// Click event for "Book Now" button
+document.getElementById('bookNowButton').addEventListener('click', function () {
+    alert('Your reservation has been booked. Enjoy your stay in the Deluxe Suite!');
+});
