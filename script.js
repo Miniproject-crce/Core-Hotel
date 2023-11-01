24043b83e596ed1a4a968d868bec08cd1e8260d8
Miniproject-crce <147860515+Miniproject-crce@users.noreply.github.com>
2023-11-01 18:53:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,8 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
-
         if (targetElement) {
             window.scrollTo({
                 top: targetElement.offsetTop - 60, // Adjust for header height
@@ -14,7 +12,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
-
 // Calculate total price for booking
 function calculateTotalPrice(roomRate) {
     const checkinDate = new Date(document.getElementById('checkinDate').value);
